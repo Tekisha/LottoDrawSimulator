0838e82e073fb651bddda879bbc9e4822b3568cb
Teodor Vidakovic <teodorv22@gmail.com>
2024-05-22 23:11:09 +0200

    feat: implement PlayerClient

---
+++ PlayerClient/PlayerClient/PlayerCallback.cs
@@ -0,0 +1,17 @@
+﻿using PlayerClient.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerClient
+{
+    public class PlayerCallback : IPlayerServiceCallback
+    {
+        public void NotifyDrawnNumbers(int[] drawnNumbers)
+        {
+            Console.WriteLine($"Drawn numbers: {string.Join(", ", drawnNumbers)}");
+        }
+    }
+}
---
+++ PlayerClient/PlayerClient/Program.cs
@@ -0,0 +1,33 @@
+﻿using PlayerClient.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InstanceContext instanceContext = new InstanceContext(new PlayerCallback());
+            PlayerServiceClient client = new PlayerServiceClient(instanceContext);
+
+            Console.WriteLine("Welcome to the LottoDrawSimulator!");
+            Console.Write("Enter your first number (0-10): ");
+            int number1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter your second number (0-10): ");
+            int number2 = int.Parse(Console.ReadLine());
+            Console.Write("Enter the amount of money you want to bet: ");
+            decimal amount = decimal.Parse(Console.ReadLine());
+
+            string playerName = client.InitPlayer(number1, number2, amount);
+            Console.WriteLine($"Player registered with name: {playerName}");
+
+            Console.WriteLine("Waiting for drawn numbers...");
+            Console.ReadLine();
+        }
+    }
+}
