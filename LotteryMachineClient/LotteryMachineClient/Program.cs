712819ea9837ef39e575a621aae577b2927e0e93
Teodor Vidakovic <teodorv22@gmail.com>
2024-05-22 23:10:14 +0200

    feat: implement LotteryMacgineClient

---
+++ LotteryMachineClient/LotteryMachineClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using LotteryMachineClient.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LotteryMachineClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChannelFactory<ILotteryMachineService> factory = new ChannelFactory<ILotteryMachineService>(new WSHttpBinding(), new EndpointAddress("http://localhost:8080/LotteryMachineService"));
+            ILotteryMachineService proxy = factory.CreateChannel();
+
+            Console.WriteLine("Starting the Lottery Machine...");
+            proxy.StartDrawing();
+
+            Console.WriteLine("Lottery Machine is running. Press [Enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
